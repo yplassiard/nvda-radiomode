ee348ee78f5b4af694ea9f30682d66fc4e335e48
Yannick PLASSIARD <plassiardyannick@gmail.com>
2018-10-24 18:35:09 +0200

    Initial support for cart categories, and VLC library backend

--- addon/globalPlugins/radiomode/__init__.py
+++ addon/globalPlugins/radiomode/__init__.py
@@ -9,6 +9,7 @@ import nvwave
 import sys, os, re, threading
 sys.path.append(os.path.dirname(__file__))
 import playsound
+import vlc
 del sys.path[-1]
 addonHandler.initTranslation()
 
@@ -20,7 +21,7 @@ class FileChooserDialog(threading.Thread):
     def run(self):
         dlg = wx.FileDialog(parent=gui.mainFrame)
         if dlg.ShowModal() == wx.ID_OK:
-            self.gp.carts[self.cart] = dlg.GetPath()
+            self.gp.carts[self.gp.category][self.cart] = dlg.GetPath()
         
 
 
@@ -41,9 +42,15 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     monitorAudioDevice = None
     speechAudioDevice = None
     carts = {}
+    categories = []
+    categoryIndex = None
+    category = ""
     players = {}
     
 
+    def generateKey(self, device, sound):
+        return "_".join([device, sound])
+    
     def __init__(self):
         super(globalPluginHandler.GlobalPlugin, self).__init__()
         logHandler.log.info("Loading radio mode")
@@ -53,7 +60,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
         self.saveConfiguration()
         
     def loadConfiguration(self):
-        addonConfig = config.conf.get("radiomode")
+        addonConfig = config.conf.get("radiomode", None)
         if addonConfig is None:
             addonConfig = {}
         self.audioDevices = nvwave.getOutputDeviceNames()
@@ -66,33 +73,59 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
         self.monitorAudioDevice = addonConfig.get("monitorAudioDevice", None)
         if self.monitorAudioDevice is None or self.monitorAudioDevice not in self.audioDevices:
             self.monitorAudioDevice = self.audioDevices[0]
-        for i in xrange(1, 13):
-            cart = addonConfig.get("cart_%s" % i, None)
-            if cart is not None and os.access(cart, os.F_OK) == True:
-                self.carts[str(i)] = cart
-                logHandler.log.info("Loaded cart {i}: {cart}".format(i=i, cart=cart))
+        for k in addonConfig.iteritems():
+            key,value = k
+            if isinstance(value, dict):
+                self.carts[key] = {}
+                for i in xrange(1, 13):
+                    cart = value.get("cart_%s" % i, None)
+                    if cart is not None and os.access(cart, os.F_OK) == True:
+                        self.carts[key][str(i)] = cart
+                        logHandler.log.info("Loaded cart {i}: {cart}".format(i=i, cart=cart.decode('utf8', 'ignore')))
 
-        logHandler.log.info("Speech audio device: %s\nMonitor audio device: %s\nMusic audio device: %s" %(self.speechAudioDevice, self.monitorAudioDevice, self.musicAudioDevice))
+        if len(self.carts) == 0:
+            self.carts[_("default")] = {}
+        self.categoryIndex = 0
+        self.categories = self.carts.keys()
+        self.category = self.categories[self.categoryIndex]
+        logHandler.log.info("Speech audio device: %s\nMonitor audio device: %s\nMusic audio device: %s\nCategory: %s" %(self.speechAudioDevice, self.monitorAudioDevice, self.musicAudioDevice, self.category))
         
     def saveConfiguration(self):
         config.conf["radiomode"] = {}
         for key in self.carts:
-            config.conf["radiomode"]["cart_%s" % key] = self.carts[key]
+            config.conf["radiomode"][key] = {}
+            for cart in self.carts[key]:
+                config.conf["radiomode"][key]["cart_%s" % cart] = self.carts[key][cart] if isinstance(self.carts[key], dict) else None
         config.conf["radiomode"]["monitorAudioDevice"] = self.monitorAudioDevice
         config.conf["radiomode"]["musicAudioDevice"] = self.musicAudioDevice
         config.conf["radiomode"]["speechAudioDevice"] = self.speechAudioDevice    
                 
+
     def bindRadioModeGestures(self):
         for key in self.fnKeys:
             self.bindGesture("kb:%s" % key, "playFile")
             self.bindGesture("kb:control+%s" % key, "loadFile")
             self.bindGesture("kb:shift+%s" % key, "sayFile")
             self.bindGesture("kb:alt+%s" % key, "previewFile")
+        self.bindGesture("kb:downarrow", "nextCategory")
+        self.bindGesture("kb:uparrow", "previousCategory")
+        self.bindGesture("kb:control+n", "newCategory")
+        self.bindGesture("kb:delete", "removeCategory")
+
     def clearRadioModeGestures(self):
         self.clearGestureBindings()
         self.bindGestures(self.__gestures)
-        
+
+    def joinDeadThreads(self):
+        for key in self.players.keys():
+            thread = self.players[key]
+            if thread is not None and thread.isStarted() == True:
+                thread.join()
+                del self.players[key]
+                thread = None
+    
     def script_cycleSpeechDevice(self, gesture):
+        self.joinDeadThreads()
         self.speechAudioDevice = self.selectDevice(self.speechAudioDevice, _("speech device"), skipSoundMapper=False, speak=False)
         config.conf["speech"]["outputDevice"] = self.speechAudioDevice
         synth = synthDriverHandler.setSynth(config.conf["speech"]["synth"])
@@ -102,11 +135,13 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 
     def script_cycleMusicDevice(self, gesture):
+        self.joinDeadThreads()
         self.musicAudioDevice = self.selectDevice(self.musicAudioDevice, _("music device"), skipSoundMapper=True)
     script_cycleMusicDevice.__doc__ = _("Changes the music audio device.")
 
 
     def script_cycleMonitorDevice(self, gesture):
+        self.joinDeadThreads()
         self.monitorAudioDevice = self.selectDevice(self.monitorAudioDevice, _("Monitor device"), skipSoundMapper=True)
     script_cycleMonitorDevice.__doc__ = _("Changes the monitor audio device.")
 
@@ -119,7 +154,10 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
         if len(audioDevices) <= 1:
             ui.message(_("Current and only output device {name}".format(audioDevices[0])))
             return audioDevices[0]
-        newIdx = (audioDevices.index(deviceName) + 1) % len(audioDevices)
+        try:
+            newIdx = (audioDevices.index(deviceName) + 1) % len(audioDevices)
+        except ValueError:
+            newIdx = 0
         if skipSoundMapper and newIdx == 0 and len(audioDevices) > 1:
             newIdx = 1
         if speak is True:
@@ -130,6 +168,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
     
     def script_toggleRadioMode(self, gesture):
+        self.joinDeadThreads()
         if self.rmActive:
             self.clearRadioModeGestures()
             self.rmActive = False
@@ -139,6 +178,21 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
             self.rmActive = True
             ui.message(_("Radio mode on"))
 
+    def script_nextCategory(self, gesture):
+        self.categoryIndex = (self.categoryIndex + 1) % len(self.categories)
+        self.category = self.categories[self.categoryIndex]
+        ui.message(self.category)
+    def script_previousCategory(self, gesture):
+        self.categoryIndex = (self.categoryIndex - 1) % len(self.categories)
+        self.category = self.categories[self.categoryIndex]
+        ui.message(self.category)
+
+    def script_newCategory(self, gesture):
+        pass
+
+    def script_removeCategory(self, gesture):
+        pass
+    
     def script_loadFile(self, gesture):
         m = re.match(".*f([0-9]+)$", gesture.identifiers[0])
         if m:
@@ -147,65 +201,73 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
     script_loadFile.__doc__ = _("Loads an audio file into the specified cart.")
 
     def script_playFile(self, gesture):
+        self.joinDeadThreads()
         m = re.match(".*f([0-9]+)$", gesture.identifiers[0])
         if m:
             cart = m.group(1)
-            file = self.carts.get(cart, None)
+            file = self.carts[self.category].get(cart, None)
             if file is None:
                 ui.message(_("No file associated to cart {cart}".format(cart=cart)))
                 return
-            p = self.players.get(cart, None)
+            playerKey = self.generateKey(self.musicAudioDevice, file)
+            p = self.players.get(playerKey, None)
             if p is None:
                 try:
                     p = playsound.Player(file)
                 except Exception as e:                    
                     logHandler.log.exception("Failed to open %s: %s" % (file, e))
                     return
-                self.players[cart] = p
+                self.players[playerKey] = p
             try:
-                p.play(nvwave.outputDeviceNameToID(self.musicAudioDevice))
+                p.setOutputDevice(nvwave.outputDeviceNameToID(self.musicAudioDevice))
+                p.start() if p.isStarted() == False else p.play()
             except Exception as e:
                 logHandler.log.exception("Failed to play %s: %s" %(file, e))
+                p.join()
                 del p
-                self.players[cart] = None
+                self.players[playerKey] = None
                 p = None
             
 
     script_playFile.__doc__ = _("Plays the file associated to the specified cart on the selected music audio device.")
     
     def script_sayFile(self, gesture):
+        self.joinDeadThreads()
         m = re.match(".*f([0-9]+)$", gesture.identifiers[0])
         if m:
             cart = m.group(1)
-            ui.message(_("Cart {i} file: {file}".format(i=cart, file=self.carts.get(cart, None))))
+            ui.message(_("Cart {i} file: {file}".format(i=cart, file=self.carts[self.category].get(cart, None).decode('utf-8', 'replace'))))
     script_sayFile.__doc__ = _("Speaks the filename associated to this cart.")
 
     def script_previewFile(self, gesture):
+        self.joinDeadThreads()
         m = re.match(".*f([0-9]+)$", gesture.identifiers[0])
         if m:
             cart = m.group(1)
-            file = self.carts.get(cart, None)
+            file = self.carts[self.category].get(cart, None)
             if file is None:
                 ui.message(_("No file associated to cart {cart}".format(cart=cart)))
                 return
-            p = self.players.get(cart, None)
+            playerKey = self.generateKey(self.monitorAudioDevice, file)
+            p = self.players.get(playerKey, None)
             if p is None:
                 try:
                     p = playsound.Player(file)
                 except Exception as e:                    
                     logHandler.log.exception("Failed to open %s: %s" % (file, e))
                     return
-                self.players[cart] = p
+                self.players[playerKey] = p
             try:
-                p.play(nvwave.outputDeviceNameToID(self.monitorAudioDevice))
+                if p.isPlaying():
+                    p.pause()
+                p.setOutputDevice(nvwave.outputDeviceNameToID(self.monitorAudioDevice))
+                p.start() if p.isStarted() == False else p.play()
             except Exception as e:
-                logHandler.log.exception("Failed to play %s: %s" %(file, e))
-                del p
-                self.players[cart] = None
-                p = None
+                logHandler.log.exception("Failed to preview %s: %s" %(file, e))
             
                 
     script_previewFile.__doc__ = _("Plays the loaded file on the selected monitor audio device.")
+
     __gestures = {
         "kb:nvda+alt+s": "cycleSpeechDevice",
         "kb:nvda+alt+o": "cycleMonitorDevice",
--- addon/globalPlugins/radiomode/playsound.py
+++ addon/globalPlugins/radiomode/playsound.py
@@ -1,3 +1,4 @@
+import logHandler
 from ctypes import c_buffer, windll
 from random import random
 from time import sleep
@@ -10,68 +11,129 @@ class Player(threading.Thread):
     playing = False
     position = 0
     alias = None
+    shouldQuit = False
+    started = False
+    loaded = False
+    deviceId = 1
     
     
     def __init__(self, sound):
         super(Player, self).__init__()
-        self.load(sound)
+        self.soundFile = sound
     
     def winCommand(self, *command):
         """Sends a Command to the WinMM interface."""
         buf = c_buffer(255)
         command = ' '.join(command).encode(getfilesystemencoding())
         errorCode = int(windll.winmm.mciSendStringA(command, buf, 254, 0))
+        logHandler.log.info(u"Sending command: " + command)
         if errorCode:
             errorBuffer = c_buffer(255)
             windll.winmm.mciGetErrorStringA(errorCode, errorBuffer, 254)
             exceptionMessage = ('\n    Error ' + str(errorCode) + ' for command:'
-                                '\n        ' + command.decode() +
-                                '\n    ' + errorBuffer.value.decode())
+                                '\n        ' + repr(command) +
+                                '\n    ' + repr(errorBuffer.value))
             raise PlaysoundException(exceptionMessage)
         return buf.value
 
     def load(self, sound):
         if self.playing is True:
-            self.stop()
+            self.stopPlayback()
         if self.alias is not None:
+            self.closeDevice()
+        try:
+            self.alias = 'playsound_' + str(random())
+            self.winCommand('open "' + sound + '" alias', self.alias)
+            self.winCommand('set', self.alias, 'time format milliseconds')
+            self.durationInMS = self.winCommand('status', self.alias, 'length')
+            self.position = self.durationInMS
+            self.loaded = True
+            logHandler.log.info("Sound ready: %s" % sound)
+            return True
+        except Exception as e:
+            logHandler.log.exception("Failed to initialize sound %s" % e)
+            return False
+
+
+    def closeDevice(self):
+        try:
             self.winCommand('close', self.alias)
-            self.alias = None
-        self.alias = 'playsound_' + str(random())
-        self.winCommand('open "' + sound + '" alias', self.alias)
-        self.winCommand('set', self.alias, 'time format milliseconds')
-        self.durationInMS = self.winCommand('status', self.alias, 'length')
-        return True
-    def play(self, deviceId=-1, fromStart=True):
+        except:
+            pass
+        self.position = 0
+        self.alias = None
+        self.isPlaying = False
+        self.loaded = False
+
+    def play(self, fromStart=True):
+        if self.loaded is False:
+            return False
         if self.isPlaying is True:
-            self.stop()
+            self.stopPlayback()
         if fromStart is True:
             position = 0
         else:
-            position = self.curPosition
-        self.winCommand('set', self.alias, 'output', str(deviceId))
-        self.winCommand('play', self.alias, 'from ', str(position), 'to', self.durationInMS.decode())
+            position = self.position
+        try:
+            self.winCommand('set', self.alias, 'output', str(self.deviceId))
+            self.winCommand('play', self.alias, 'from ', str(position), 'to', self.durationInMS.decode())
+        except Exception as e:
+            logHandler.log.exception("Failed to start playback: %s" % e)
+            return False
         self.playing = True
 
-    def stop(self):
-        self.winCommand('stop', self.alias)
-        self.curPosition = 0
+    def stopPlayback(self):
+        try:
+            self.winCommand('stop', self.alias)
+        except Exception as e:
+            logHandler.log.error("Failed to stop sound playback: %s" % e)
+            pass
         self.playing = False
+        
     def pause(self):
         if self.playing:
-            self.winCommand('pause', self.alias)
+            try:
+                self.winCommand('pause', self.alias, 'wait')
+            except:
+                pass
             self.playing = False
+
+    def setOutputDevice(self, deviceId):
+        self.deviceId = deviceId
+    def isStarted(self):
+        return self.started
     
         
     def run(self):
+        self.started = True
+        logHandler.log.info("Starting new playsound thread")
+        self.load(self.soundFile) if self.loaded == False else None
+        while self.loaded is False:
+            sleep(0.01)
+        try:
+            self.play()
+        except:
+            logHandler.log.info("Failed to start playing sound")
+            self.started = False
+            return False
         while self.shouldQuit is False:
-            time.sleep(0.1)
-            position = self.winCommand('status', self.alias, 'position')
-            if position == self.curPosition:
+            sleep(0.01)
+            try:
+                position = self.winCommand('status', self.alias, 'position')
+            except Exception as e:
+                logHandler.log.exception("Thread exception: %s"%(e))
+                position = -1
+            logHandler.log.info("Curpos: %s, last=%s"%(position, self.durationInMS))
+            if int(position) >= int(self.durationInMS):
                 self.playing = False
-            self.curPosition = position
-        self.stop()
-        self.winCommand('close', self.alias)
-        self.position = 0
+                self.shouldQuit = True
+            self.position = position
+
+        self.started = False
+        logHandler.log.info("Sound %s finished playback" % self.alias)
+        self.stopPlayback()
+        self.closeDevice()
+
         self.alias = None
 
     def isPlaying(self):
